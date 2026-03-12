387f13d187aa5ff0b1c8ce0f920f2d7e29463fbf
AbdElrhman AbdElfattah <abdelrhmanabdelfattah9866@gmail.com>
2026-03-12 21:34:49 +0200

    add initial payment

--- lib/payment/payment_screen.dart
+++ lib/payment/payment_screen.dart
@@ -1,15 +1,15 @@
 import 'package:flutter/material.dart';
 
-class LoginScreen extends StatelessWidget {
-  const LoginScreen({super.key});
+class PaymentScreen extends StatelessWidget {
+  const PaymentScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         leading: const Icon(Icons.arrow_back, size: 34, color: Colors.white,),
-        title: const Text("Login",style: TextStyle(fontSize: 24,
-        fontWeight: FontWeight.bold,color: Colors.white),),
+        title: const Text("PaymentScreen",style: TextStyle(fontSize: 24,
+            fontWeight: FontWeight.bold,color: Colors.white),),
         centerTitle: true,
         backgroundColor: Colors.red,
       ),
@@ -20,12 +20,12 @@ class LoginScreen extends StatelessWidget {
             height: 64,
             width: double.infinity,
             decoration: BoxDecoration(
-              color: Colors.red,
-              borderRadius: BorderRadius.circular(12)
+                color: Colors.red,
+                borderRadius: BorderRadius.circular(12)
             ),
             child: const Center(
-              child: Text("Login SuccessFully", style: TextStyle(
-                fontSize: 22, fontWeight: FontWeight.w600,color: Colors.white
+              child: Text("Payment SuccessFully", style: TextStyle(
+                  fontSize: 22, fontWeight: FontWeight.w600,color: Colors.white
               ),),
             ),
           )
